b8f6453988dd1bebf9a9110f148a0d85c62f7efc
= <=>
2023-03-14 14:33:21 -0300

    version updated

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,34 +1,38 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
 
 contract Lottery {
     address public manager;
-    address[] public players;
-    
-    function Lottery() public {
+    address payable[] public players;
+
+    constructor(){
         manager = msg.sender;
     }
-    
+
     function enter() public payable {
         require(msg.value > .01 ether);
-        players.push(msg.sender);
+        
+        players.push(payable(msg.sender));
     }
-    
+
     function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
+        return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players)));
     }
-    
+
     function pickWinner() public restricted {
         uint index = random() % players.length;
-        players[index].transfer(this.balance);
-        players = new address[](0);
+        address payable winner = payable(players[index]);
+        winner.transfer(address(this).balance);
+        players = new address payable[](0);
     }
-    
+
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
-    function getPlayers() public view returns (address[]) {
+
+    function getPlayers() public view returns (address payable[] memory) {
         return players;
     }
-}   
+}
--- deploy.js
+++ deploy.js
@@ -1,24 +1,25 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'bamboo flush orphan leaf gallery top design hobby top slide draw arrow',
-  'https://goerli.infura.io/v3/7766990a7062406ab93ff7ad7fca4131'
+    'bamboo flush orphan leaf gallery top design hobby top slide draw arrow',
+    'https://goerli.infura.io/v3/7766990a7062406ab93ff7ad7fca4131'
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
+    const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+    console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object })
+        .send({ gas: '1000000', from: accounts[0] });
 
-  console.log(interface);
-  console.log('Contract deployed to', result.options.address);
-  provider.engine.stop();
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
 };
+
 deploy();
